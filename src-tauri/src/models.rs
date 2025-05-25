ef5a2cff52f5291ead90a87a332d748734657403
pa <maplenagisa@gmail.com>
2025-05-25 12:36:45 +0900

    deps and login models

---
+++ src-tauri/src/models.rs
@@ -0,0 +1,43 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct LoginCrendentials {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Serialize, Debug, Clone)]
+pub struct VrcErrorDetail {
+    pub message: String,
+    pub status_code: u16,
+}
+
+#[derive(Serialize, Debug, Clone)]
+pub struct VrcErrorResponse {
+    pub error: VrcErrorDetail,
+}
+
+#[derive(Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct VrcCurrentUser{
+    pub id: String,
+    pub username: String,
+    pub display_name: String,
+    pub bio: Option<String>,
+    pub current_avatar_thumbnail_image_url: Option<String>,
+    pub status : Option<String>,
+    pub last_login: Option<String>, // ISO 8601 DataTime string
+    pub email_verified: Option<bool>,
+    // login: "emailOtp", "?"(2FA), null
+    pub requires_two_factor_auth: Option<String>,
+
+    // ... more fields, not all will be used, just write these for now.
+
+}
+
+#[derive(Debug, Clone)]
+pub struct AuthContext {
+    pub user: VrcCurrentUser,
+    pub auth_cookie_value: String,
+
+}
