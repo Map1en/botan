c83ae15cbfd3d38cee8230d71abbbb5601011619
pa <maplenagisa@gmail.com>
2025-05-25 16:19:08 +0900

    rm chrono

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,7 +2,7 @@ pub mod commands;
 pub mod core_logic;
 pub mod models;
 
-use commands::{login, mock_login};
+use commands::{login};
 
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -10,7 +10,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_store::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![login, mock_login])
+        .invoke_handler(tauri::generate_handler![login])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
