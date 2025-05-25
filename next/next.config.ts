ddb1eea55147a6d6a3538cb04417a69178c56368
pa <maplenagisa@gmail.com>
2025-05-25 21:38:23 +0900

    rename src

--- next/next.config.ts
+++ next/next.config.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+
 const isProd = process.env.NODE_ENV === 'production';
 
 const internalHost = process.env.TAURI_DEV_HOST || 'localhost';
@@ -7,7 +9,7 @@ const nextConfig = {
   // Ensure Next.js uses SSG instead of SSR
   // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
   output: 'export',
-  distDir: 'dist',
+  distDir: '../dist',
   // Note: This feature is required to use the Next.js Image component in SSG mode.
   // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
   images: {
@@ -15,6 +17,11 @@ const nextConfig = {
   },
   // Configure assetPrefix or else the server won't properly resolve your assets.
   assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'next');
+
+    return config;
+  },
 };
 
 export default nextConfig;
