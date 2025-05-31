5ed261cf84392617566957eefd30df5f413bc1a9
pa <maplenagisa@gmail.com>
2025-05-31 17:15:46 +0900

    store cookie

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -3,6 +3,10 @@ use api_models::EitherUserOrTwoFactor;
 use botan_core::api_models;
 use botan_core::auth::login;
 use botan_core::models::LoginCredentials;
+use reqwest::cookie::CookieStore;
+use reqwest::Url;
+use serde_json::json;
+use std::str::FromStr;
 
 #[tauri::command]
 pub async fn auth_user(
@@ -11,6 +15,11 @@ pub async fn auth_user(
 ) -> Result<EitherUserOrTwoFactor, String> {
     log::info!("Tauri command, api - 'auth/user', login");
 
+    log::info!(
+        "Cookie store: {:?}",
+        state.store.lock().unwrap().get("cookies")
+    );
+
     let basic_auth_data = (
         credentials.username.clone(),
         Some(credentials.password.clone()),
@@ -18,9 +27,32 @@ pub async fn auth_user(
 
     let mut login_config = state.vrc_client.config.clone();
 
+    let cookie_store = std::sync::Arc::new(reqwest::cookie::Jar::default());
+    login_config.client = reqwest::Client::builder()
+        .cookie_provider(cookie_store.clone())
+        .build()
+        .unwrap();
+
     login_config.basic_auth = Some(basic_auth_data);
 
     let login_result = login(&login_config).await;
+
+    if let Ok(url) = Url::from_str("https://api.vrchat.cloud") {
+        if let Some(cookie) = cookie_store.cookies(&url) {
+            if let Ok(cookie_str) = cookie.to_str() {
+                log::info!("Cookies: {}", cookie_str);
+                state
+                    .store
+                    .lock()
+                    .unwrap()
+                    .set("cookies", json!({"cookie":&cookie_str}))
+            } else {
+                log::warn!("Failed to convert cookies to string");
+            }
+        }
+    } else {
+        log::warn!("Failed to parse URL");
+    }
 
     match login_result {
         Ok(user_data) => {
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,13 @@
 pub mod commands;
 use botan_core::client::VrcApiClient;
+use serde_json::json;
+use std::sync::{Arc, Mutex};
+use tauri::{Manager, Wry};
+use tauri_plugin_store::{Store, StoreExt};
 
 pub struct AppState {
     vrc_client: VrcApiClient,
+    store: Arc<Mutex<Arc<Store<Wry>>>>,
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -10,15 +15,22 @@ pub fn run() {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let vrc_client_instance = VrcApiClient::new();
-    let app_state = AppState {
-        vrc_client: vrc_client_instance,
-    };
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .setup(|app| {
+            let store = app.store("store.json")?;
+            store.set("some-key", json!({ "value": 5 }));
+            let app_state = AppState {
+                vrc_client: vrc_client_instance,
+                store: Arc::new(Mutex::new(store)),
+            };
+            app.manage(app_state);
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .invoke_handler(tauri::generate_handler![commands::auth_user])
-        .manage(app_state)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
