525c651bb435a63026b2320bd86d785da4429ac7
pa <maplenagisa@gmail.com>
2025-06-06 15:09:19 +0900

    impl Default

--- core/src/client.rs
+++ core/src/client.rs
@@ -11,17 +11,21 @@ pub struct VrcApiClient {
     pub config: Configuration,
 }
 
-impl VrcApiClient {
-    pub fn new() -> Self {
+impl Default for VrcApiClient {
+    fn default() -> Self {
         let mut config = Configuration::default();
         const PKG_NAME: &str = env!("CARGO_PKG_NAME");
         const PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
-        pub(crate) fn get_default_user_agent() -> String {
-            format!("{}/{}", PKG_NAME, PKG_VERSION)
-        }
-        config.user_agent = Some(get_default_user_agent());
+
+        config.user_agent = Some(format!("{}/{}", PKG_NAME, PKG_VERSION));
 
         Self { config }
+    }
+}
+
+impl VrcApiClient {
+    pub fn new() -> Self {
+        Self::default()
     }
 }
 
